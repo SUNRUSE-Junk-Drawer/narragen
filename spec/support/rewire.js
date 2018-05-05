60ef5d850ed7acf7c2d5f7223c3506789fe6ed6a
jameswilddev <jameswilddev@outlook.com>
2018-05-05 09:57:24 +0100

    Added Rewire.

---
+++ spec/support/rewire.js
@@ -0,0 +1,14 @@
+const rewire = require("rewire")
+const index = rewire("../../dist/index")
+
+global.get = name => index.__get__(name)
+global.set = (name, valueFactory) => {
+  let replaced
+  beforeEach(() => {
+    replaced = index.__get__(name)
+    index.__set__(name, valueFactory())
+  })
+  afterEach(() => {
+    index.__set__(name, replaced)
+  })
+}
